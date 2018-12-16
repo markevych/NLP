bd571a3504d20c79c352de47808ef596be1ccac1
Oleh Markevych <oleh.markevych@gmail.com>
2018-12-17 01:15:06 +0200

    Fix bugs and finish bot

--- dialogflow-chatbot/src/app/app.module.ts
+++ dialogflow-chatbot/src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChatModule } from './chat/chat.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
+  imports: [    
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
     AppRoutingModule,
     ChatModule
+  ],
+  declarations: [
+    AppComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
--- dialogflow-chatbot/src/app/chat/chat-dialog/chat-dialog.component.ts
+++ dialogflow-chatbot/src/app/chat/chat-dialog/chat-dialog.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import 'rxjs/add/operator/scan';
+import { Message, ChatService } from 'src/app/chat.service';
+import { Observable } from 'rxjs';
+
 
 @Component({
-  selector: 'app-chat-dialog',
+  selector: 'chat-dialog',
   templateUrl: './chat-dialog.component.html',
   styleUrls: ['./chat-dialog.component.css']
 })
 export class ChatDialogComponent implements OnInit {
 
-  constructor() { }
+  messages: Observable<Message[]>;
+  formValue: string;
+
+  constructor(public chat: ChatService) { }
 
   ngOnInit() {
+    // appends to array after each new message is added to feedSource
+    this.messages = this.chat.conversation.asObservable()
+        .scan((acc, val) => acc.concat(val) );
+  }
+
+  sendMessage() {
+    this.chat.converse(this.formValue);
+    this.formValue = '';
   }
 
 }
